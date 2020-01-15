056a583def79dba1f54312c4b36ce626989598ad
hunterwb <hunter@hunterwb.com>
2020-01-15 10:58:08 -0800

    Start python setup

---
+++ python/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    author="Hunter WB",
+    author_email="hunter@hunterwb.com",
+    name='anyascii',
+    version='0.1.0-dev',
+    description='Unicode to ASCII transliteration',
+    url='https://github.com/hunterwb/any-ascii',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.3",
+    zip_safe=True,
+)
