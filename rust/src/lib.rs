4551aeec7b85a0ec30e8f38e109afa97d047854b
hunterwb <hunter@hunterwb.com>
2020-02-10 01:34:50 -0800

    Rust unsafe slice

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -40,7 +40,7 @@ pub fn any_ascii_char(c: char) -> &'static str {
         }
         if len <= 3 {
             unsafe {
-                std::str::from_utf8_unchecked(&ptr[..len])
+                std::str::from_utf8_unchecked(ptr.get_unchecked(..len))
             }
         } else {
             let i = (((ptr[0] as u16) << 8) | (ptr[1] as u16)) as usize;
