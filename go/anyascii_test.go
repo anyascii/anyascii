e56bc906d674f1f96a0d1f1b865e6ba046ffa905
hunterwb <hunter@hunterwb.com>
2020-02-15 14:06:09 -0800

    Start Go tests

---
+++ go/anyascii_test.go
@@ -0,0 +1,32 @@
+package anyascii
+
+import "testing"
+
+func TestTransliterate(t *testing.T) {
+	check(t, "", "")
+	check(t, "René François Lacôte", "Rene Francois Lacote")
+	check(t, "Großer Hörselberg", "Grosser Horselberg")
+	check(t, "Trần Hưng Đạo", "Tran Hung Dao")
+	check(t, "Nærøy", "Naeroy")
+	check(t, "Φειδιππίδης", "Feidippidis")
+	check(t, "Δημήτρης Φωτόπουλος", "Dimitris Fotopoylos")
+	check(t, "Борис Николаевич Ельцин", "Boris Nikolaevich El'tsin")
+	check(t, "אברהם הלוי פרנקל", "'vrhm hlvy frnkl")
+	check(t, "სამტრედია", "samt'redia")
+	check(t, "Աբովյան", "Abovyan")
+	check(t, "深圳", "ShenZhen")
+	check(t, "深水埗", "ShenShuiBu")
+	check(t, "화성시", "hwaseongsi")
+	check(t, "華城市", "HuaChengShi")
+	check(t, "さいたま", "saitama")
+	check(t, "埼玉県", "QiYuXian")
+	check(t, "トヨタ", "toyota")
+	check(t, "⠠⠎⠁⠽⠀⠭⠀⠁⠛", "^say x ag")
+}
+
+func check(t *testing.T, s string, expected string) {
+	actual := Transliterate(s)
+	if actual != expected {
+		t.Errorf("Expected <%s> got <%s>", expected, actual)
+	}
+}
