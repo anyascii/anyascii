9c7df033bc057510934691082a04202766860eae
hunterwb <hunter@hunterwb.com>
2020-02-15 14:21:59 -0800

    Start Ruby tests

---
+++ ruby/lib/any_ascii_test.rb
@@ -0,0 +1,30 @@
+require "test/unit"
+require_relative './any_ascii.rb'
+
+class AnyAsciiTest < Test::Unit::TestCase
+	def test_transliterate
+		check("", "")
+		check("René François Lacôte", "Rene Francois Lacote")
+		check("Großer Hörselberg", "Grosser Horselberg")
+		check("Trần Hưng Đạo", "Tran Hung Dao")
+		check("Nærøy", "Naeroy")
+		check("Φειδιππίδης", "Feidippidis")
+		check("Δημήτρης Φωτόπουλος", "Dimitris Fotopoylos")
+		check("Борис Николаевич Ельцин", "Boris Nikolaevich El'tsin")
+		check("אברהם הלוי פרנקל", "'vrhm hlvy frnkl")
+		check("სამტრედია", "samt'redia")
+		check("Աբովյան", "Abovyan")
+		check("深圳", "ShenZhen")
+		check("深水埗", "ShenShuiBu")
+		check("화성시", "hwaseongsi")
+		check("華城市", "HuaChengShi")
+		check("さいたま", "saitama")
+		check("埼玉県", "QiYuXian")
+		check("トヨタ", "toyota")
+		check("⠠⠎⠁⠽⠀⠭⠀⠁⠛", "^say x ag")
+	end
+
+	def check(s, expected)
+		assert_equal(expected, AnyAscii.transliterate(s))
+	end
+end
