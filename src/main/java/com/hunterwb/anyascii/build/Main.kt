3866d8a316048436bb5950528b768809399ca45f
hunterwb <hunter@hunterwb.com>
2020-02-18 00:56:40 -0800

    Start Lao

--- src/main/java/com/hunterwb/anyascii/build/Main.kt
+++ src/main/java/com/hunterwb/anyascii/build/Main.kt
@@ -86,6 +86,7 @@ private fun custom() = Table()
         .then(Table("squared-katakana-words"))
         .then(katakana())
         .then(hiragana())
+        .then(Table("lao"))
 
 private fun cyrillic() = Table()
         .then(Table("cyrillic"))
