82f1d55bf9bf350cc5e99fb6f418c18893ea3e51
hunterwb <hunter@hunterwb.com>
2020-02-26 15:37:21 -0800

    Add Java comment

--- java/src/main/java/com/hunterwb/anyascii/AnyAscii.java
+++ java/src/main/java/com/hunterwb/anyascii/AnyAscii.java
@@ -5,6 +5,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 
+/**
+ * Unicode to ASCII transliteration.
+ *
+ * Converts Unicode text to a reasonable representation using only ASCII.
+ *
+ * For most characters in Unicode, Any-Ascii provides an ASCII-only replacement string.
+ * Text is converted character-by-character without considering the context.
+ * The mappings for each language are based on popular existing romanization schemes.
+ * Symbolic characters are converted based on their meaning or appearance.
+ * All ASCII characters in the input are left unchanged,
+ * every other character is replaced with printable ASCII characters.
+ * Unknown characters are removed.
+ */
 public final class AnyAscii {
 
     private AnyAscii() {}
