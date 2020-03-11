dd3063ab5b53c873e1f62ccaf3e53b0626ab2f0c
hunterwb <hunter@hunterwb.com>
2020-03-11 10:09:13 -0700

    Delete GitHub emojis

--- src/main/java/com/hunterwb/anyascii/build/Emojis.kt
+++ src/main/java/com/hunterwb/anyascii/build/Emojis.kt
@@ -1,25 +1,13 @@
 package com.hunterwb.anyascii.build
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import java.io.File
 
 private val EMOJI_BLACKLIST = "©®‼⁉™Ⓜ㊗㊙🈁🈂🈚🈯🈲🈳🈴🈵🈶🈷🈸🈹🈺🉐🉑".codePointsArray().asList()
 
-fun emojis() = discordEmojis()
-
-private fun gitHubEmojis(): Table = jacksonObjectMapper()
-        .readValue<LinkedHashMap<String, String>>(File("input/github.emojis.json"))
-        .filterValues { it.contains("/unicode/") && '-' !in it }
-        .mapKeys { ":${it.key}:" }
-        .mapValues { it.value.substringBeforeLast('.').substringAfterLast("/").toInt(16) }
-        .entries
-        .associateTo(Table()) { it.value to it.key }
-        .minus(EMOJI_BLACKLIST)
-
-private fun discordEmojis() = jacksonObjectMapper()
+fun emojis() = jacksonObjectMapper()
         .readTree(File("input/discord.emojis.json"))
-        .flatMap { it }
+        .flatten()
         .filter { it["surrogates"].asText().codePointsArray().dropLastWhile { it == 0xfe0f }.size == 1 }
         .associateTo(Table()) { it["surrogates"].asText().codePointAt(0) to it["names"].first().asText().let { ":$it:" } }
         .minus(EMOJI_BLACKLIST)
