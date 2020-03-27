d906535888c4d43fb12d0786afa9d437fb193933
hunterwb <hunter@hunterwb.com>
2020-03-27 14:15:20 -0700

    Rename emojis file

--- src/main/java/com/hunterwb/anyascii/build/Emojis.kt
+++ src/main/java/com/hunterwb/anyascii/build/Emojis.kt
@@ -6,7 +6,7 @@ import java.io.File
 private val EMOJI_BLACKLIST = "©®‼⁉™Ⓜ㊗㊙🈁🈂🈚🈯🈲🈳🈴🈵🈶🈷🈸🈹🈺🉐🉑".codePointsArray().asList()
 
 fun emojis() = jacksonObjectMapper()
-        .readTree(File("input/discord.emojis.json"))
+        .readTree(File("input/discord-emojis.json"))
         .flatten()
         .filter { it["surrogates"].asText().codePointsArray().dropLastWhile { it == 0xfe0f }.size == 1 }
         .associateTo(Table()) { it["surrogates"].asText().codePointAt(0) to it["names"].first().asText().let { ":$it:" } }
