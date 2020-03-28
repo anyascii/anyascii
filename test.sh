bb3b5564d81b9fa637821db30cad0418c3c0f8bc
hunterwb <hunter@hunterwb.com>
2020-03-27 21:47:10 -0700

    Update test script

--- test.sh
+++ test.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 npm test
-
 go test ./go
-
 ruby ruby/lib/any_ascii_test.rb
-
-cd rust
-cargo test
-cd ..
-
-cd python
-python -m pytest
-cd ..
-
-cd java
-./mvnw test
-cd ..
+cd rust && cargo test && cd -
+cd python && python -m pytest && cd -
+cd java && ./mvnw test && cd -
