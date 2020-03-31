c167312bda0159c6815a5d05c07b28b9921ba8d1
hunterwb <hunter@hunterwb.com>
2020-03-31 00:12:46 -0700

    Start Myanmar extended

--- js/data/0aa.js
+++ js/data/0aa.js
@@ -1,1 +1,1 @@
-module.exports='																																																																																0	1	2	3	4	5	6	7	8	9'.split('	')
+module.exports='																																																																																0	1	2	3	4	5	6	7	8	9							g	c	ch	j	jh	ny	tt	tth	dd	ddh	dh	n	s	h	hh	f	"	x	z	r	oay	\'n	hm	!			r				ch	sh'.split('	')
