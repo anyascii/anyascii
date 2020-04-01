95cac0c134d428f1a82ad2a1d3fb834bd1797716
hunterwb <hunter@hunterwb.com>
2020-04-01 16:52:57 -0700

    Add test batch file

---
+++ test.cmd
@@ -0,0 +1,6 @@
+call npm test || pause && exit
+go test ./go || pause && exit
+ruby ruby/lib/any_ascii_test.rb || pause && exit
+cd rust && cargo test && cd .. || pause && exit
+cd python && python -m pytest && cd .. || pause && exit
+cd java && call mvnw test && cd .. || pause && exit
