fc5fd26e5ef37d3cf9b5f3ce9ff92028df4a9b08
hunterwb <hunter@hunterwb.com>
2020-04-04 19:31:50 -0700

    C# test commands

--- test.cmd
+++ test.cmd
@@ -4,3 +4,4 @@ ruby ruby/lib/any_ascii_test.rb || pause && exit
 cd rust && cargo test && cd .. || pause && exit
 cd python && python -m pytest && cd .. || pause && exit
 cd java && call mvnw test && cd .. || pause && exit
+dotnet test csharp/test || pause && exit
--- test.sh
+++ test.sh
@@ -8,3 +8,4 @@ ruby ruby/lib/any_ascii_test.rb
 cd rust && cargo test && cd -
 cd python && python -m pytest && cd -
 cd java && ./mvnw test && cd -
+dotnet test csharp/test
