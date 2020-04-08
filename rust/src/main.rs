22f7ed9bf2d156ea6cc2c14f266e8e20044cb716
hunterwb <hunter@hunterwb.com>
2020-04-08 15:51:04 -0700

    Rust CLI help message

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,6 +8,13 @@ fn main() {
         .join(" ");
 
     match arg.as_str() {
+        "-h" | "--help" => println!(
+            "{} {}\n{}\n{}",
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION"),
+            env!("CARGO_PKG_DESCRIPTION"),
+            env!("CARGO_PKG_HOMEPAGE")
+        ),
         "-v" | "--version" => println!("{}", env!("CARGO_PKG_VERSION")),
         _ => println!("{}", any_ascii(&arg)),
     }
