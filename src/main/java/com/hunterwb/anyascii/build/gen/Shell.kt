e2a608176ce830b2ecbe0e9d985e997ca9209c1f
hunterwb <hunter@hunterwb.com>
2020-05-20 04:56:00 -0700

    sh license
    [skip ci]

--- src/main/java/com/hunterwb/anyascii/build/gen/Shell.kt
+++ src/main/java/com/hunterwb/anyascii/build/gen/Shell.kt
@@ -6,7 +6,9 @@ import java.nio.file.Path
 
 fun shell(g: Generator) {
     Files.newBufferedWriter(Path.of("sh/anyascii")).use { w ->
-        w.write("#!/bin/sh\n\n")
+        w.write("#!/bin/sh\n")
+        val license = Files.readAllLines(Path.of("LICENSE")).joinToString("") { "# $it\n" }
+        w.write("\n$license\n")
         for ((blockNum, block) in g.blocks) {
             w.write("_%03x='".format(blockNum))
             w.write(block.joinToString("\t").replace("'", "'\\''"))
