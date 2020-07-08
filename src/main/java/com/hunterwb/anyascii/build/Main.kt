69cc2824d991ece2d0a7f624ebee3d105d1f7e67
hunterwb <hunter@hunterwb.com>
2020-07-08 01:20:06 -0700

    Chorasmian

--- src/main/java/com/hunterwb/anyascii/build/Main.kt
+++ src/main/java/com/hunterwb/anyascii/build/Main.kt
@@ -294,6 +294,7 @@ private fun phoenician() = Table("phoenician")
         .then((0x10b78..0x10b7f).toTable { it.numericValue.toString() })
         .then((0x10ba9..0x10baf).toTable { it.numericValue.toString() })
         .then((0x10f1d..0x10f25).toTable { it.numericValue.toString() })
+        .then((0x10fc5..0x10fcb).toTable { it.numericValue.toString() })
 
 private val LINEAR_AB_ID = "(\\S*?)(\\d{3})(\\S*)".toRegex()
 
