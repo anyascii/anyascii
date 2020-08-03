5b9fa891492df8d7abaf162c54840e9593dae71f
hunterwb <hunter@hunterwb.com>
2020-08-03 00:59:54 -0700

    Update Python classifiers

--- python/setup.py
+++ python/setup.py
@@ -25,15 +25,8 @@ setuptools.setup(
     zip_safe=True,
     classifiers=[
         'License :: OSI Approved :: ISC License (ISCL)',
+        'Topic :: Text Processing :: General',
         'Topic :: Text Processing :: Linguistic',
-        'Topic :: Text Processing',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
     ]
 )
