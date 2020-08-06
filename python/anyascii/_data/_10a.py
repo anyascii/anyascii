88bf46ba508d3eaf6413eac7a3af62eb2e0ac9ac
hunterwb <hunter@hunterwb.com>
2020-08-06 15:13:35 -0700

    Update Old South & North Arabian

--- python/anyascii/_data/_10a.py
+++ python/anyascii/_data/_10a.py
@@ -1,1 +1,1 @@
-b="																																																																																																h1	l	h2	m	q	w	s2	r	b	t1	s1	k	n	h3	s4	s3	f	'	`	d3	g1	d1	g2	t3	z1	d2	y	t	z2	|	50	#"
+b="																																																																																																h1	l	h2	m	q	w	s2	r	b	t1	s1	k	n	h3	s4	s3	f	'	`	d3	g1	d1	g2	t3	z1	d2	y	t2	z2	|	50	#	h1	l	h2	m	q	w	s2	r	b	t1	s1	k	n	h3	s4	s3	f	'	`	d3	g1	d1	g2	t3	z1	d2	y	t2	z2	|	10	20"
--- ruby/lib/data/10a.rb
+++ ruby/lib/data/10a.rb
@@ -1,1 +1,1 @@
-module X10a B='																																																																																																h1	l	h2	m	q	w	s2	r	b	t1	s1	k	n	h3	s4	s3	f	\'	`	d3	g1	d1	g2	t3	z1	d2	y	t	z2	|	50	#'.split '	' end
+module X10a B='																																																																																																h1	l	h2	m	q	w	s2	r	b	t1	s1	k	n	h3	s4	s3	f	\'	`	d3	g1	d1	g2	t3	z1	d2	y	t2	z2	|	50	#	h1	l	h2	m	q	w	s2	r	b	t1	s1	k	n	h3	s4	s3	f	\'	`	d3	g1	d1	g2	t3	z1	d2	y	t2	z2	|	10	20'.split '	' end
