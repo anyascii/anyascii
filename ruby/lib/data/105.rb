a220d6c750f9a00046566382e14a6dae34561c43
hunterwb <hunter@hunterwb.com>
2020-08-06 04:43:05 -0700

    Update Elbasan

--- ruby/lib/data/105.rb
+++ ruby/lib/data/105.rb
@@ -1,1 +1,1 @@
-module X105 B='a	b	c	ch	d	nd	dh	ei	e	f	g	gj	h	i	j	k	l	ll	m	n	n	nj	o	p	q	r	rr	s	sh	t	th	u	v	x	y	z	zh	gh	gh	kh									a	b	g	d	e	z	e`	z^	t	c.`	y	z`	i	?	l	n\'	x	d\'	c.	j`	k.	l\'	h	x.	a`	c`	c.^	c\'	m	q.	n	j\'	s^	j^	o	t.\'	f	j	c^	p.	g.	r	s	v	t.	s`	u`	c.\'	c	w	p	k												>'.split '	' end
+module X105 B='a	b	c	C	d	nd	dh	e	E	f	g	gj	h	i	j	k	l	ll	m	n	n	nj	o	p	q	r	rr	s	sh	t	th	u	v	x	y	z	zh	G	G	X									a	b	g	d	e	z	e`	z^	t	c.`	y	z`	i	?	l	n\'	x	d\'	c.	j`	k.	l\'	h	x.	a`	c`	c.^	c\'	m	q.	n	j\'	s^	j^	o	t.\'	f	j	c^	p.	g.	r	s	v	t.	s`	u`	c.\'	c	w	p	k												>'.split '	' end
