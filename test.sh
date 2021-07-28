208455bf4e1787efdd70a9a1bc4aa58aa9f593c8
hunterwb <hunter@hunterwb.com>
2021-07-28 16:19:49 -0700

    Cleanup testing
    [skip ci]

--- test.sh
+++ test.sh
@@ -3,13 +3,13 @@
 set -eux
 cd -- "$(dirname -- "${BASH_SOURCE:-$0}")"
 
-cd js && npm test && cd -
-cd go && go test && cd -
-cd ruby && ruby lib/any_ascii_test.rb && cd -
-cd rust && cargo test && cd -
-cd python && python3 -m pytest && cd -
-cd java && mvn test && cd -
-cd csharp && dotnet test && cd -
-cd php && php test.php && cd -
-cd julia && julia --project=. -e 'using Pkg; Pkg.test()' && cd -
-cd sh && ./test.sh && cd -
+(cd js && npm test)
+(cd go && go test)
+(cd ruby && ruby lib/any_ascii_test.rb)
+(cd rust && cargo test)
+(cd python && python3 -m pytest)
+(cd java && mvn test)
+(cd csharp && dotnet test)
+(cd php && php test.php)
+(cd julia && julia --project=. -e 'using Pkg; Pkg.test()')
+(cd sh && ./test.sh)
