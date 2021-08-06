671e4a690d0cfacbf82d01e9f51acafdabf126ae
hunterwb <hunter@hunterwb.com>
2021-08-06 09:12:19 -0700

    Update subtree_push.sh

--- subtree_push.sh
+++ subtree_push.sh
@@ -3,7 +3,8 @@
 set -eux
 cd -- "$(dirname -- "${BASH_SOURCE:-$0}")"
 
-test "$(git rev-parse master)" = "$(git rev-parse origin/master)"
+git fetch
+test "$(git rev-parse HEAD)" = "$(git rev-parse origin/master)"
 
 git subtree push --prefix=go https://github.com/anyascii/go.git master
 git subtree push --prefix=php https://github.com/anyascii/php.git master
