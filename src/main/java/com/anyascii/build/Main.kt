7f98bbac74da8a453183df3e481ddc1910052981
hunterwb <hunter@hunterwb.com>
2021-11-16 16:44:38 -0800

    Start Znamenny Musical Notation

--- src/main/java/com/anyascii/build/Main.kt
+++ src/main/java/com/anyascii/build/Main.kt
@@ -231,6 +231,7 @@ private fun custom() = Table()
         .then(Table("vithkuqi"))
         .then(Table("toto"))
         .then(tangsa())
+        .then(znamennyMusicalNotation())
 
 private fun cyrillic() = Table("cyrillic")
         .cased(codePoints(UScript.CYRILLIC))
@@ -397,3 +398,5 @@ private fun enclosedIdeographicSupplement() = Table()
 private fun tangsa() = codePoints(UScript.TANGSA)
         .filterName { "LETTER" in it }
         .toTable { it.name.substringAfterLast(' ').removeSuffix("A").lower() }
+
+private fun znamennyMusicalNotation() = UnicodeBlock.ZNAMENNY_MUSICAL_NOTATION.toTable { if (" NEUME " in it.name) "-" else "" }
