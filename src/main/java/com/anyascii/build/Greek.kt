1e01562c59aef8513f572f38fc3bb27df913fcb2
hunterwb <hunter@hunterwb.com>
2021-12-22 20:47:32 -0800

    Cleanup Greek

--- src/main/java/com/anyascii/build/Greek.kt
+++ src/main/java/com/anyascii/build/Greek.kt
@@ -1,25 +1,18 @@
 package com.anyascii.build
 
+import com.ibm.icu.lang.UCharacterCategory
 import com.ibm.icu.lang.UScript
 
 fun greek() = Table()
         .then(greekMath())
         .then(Table("greek"))
+        .then(Table("greek-extended"))
         .cased(codePoints(UScript.GREEK))
-        .then(codePoints(UScript.GREEK).filterName { it.contains("WITH DASIA") }.toTable {
-            val baseName = it.name.substringBefore(" WITH")
-            val base = CodePoint(baseName).asString()
-            if ("RHO" in baseName) {
-                "${base}h"
-            } else {
-                if ("CAPITAL" in baseName) "H${base.lower()}" else "h$base"
-            }
-        })
 
-private val GREEK_MATH = ((0x1d6a8..0x1d7cb) + 0x2207 + 0x2202 + 0x3f4 + 0x3f5 + 0x3d1 + 0x3f0 + 0x3d5 + 0x3f1 + 0x3d6 + 0x3d0).toList()
+private val GREEK_MATH_LETTERS = (0x1d6a8..0x1d7cb).filter { it.category != UCharacterCategory.MATH_SYMBOL }
 
 private fun greekMath() = Table("greek-math")
         .cased(codePoints(UScript.GREEK))
-        .then(GREEK_MATH.normalize(NFKC))
+        .then(GREEK_MATH_LETTERS.normalize(NFKC))
         .transliterate()
-        .retain(GREEK_MATH)
+        .retain(GREEK_MATH_LETTERS)
