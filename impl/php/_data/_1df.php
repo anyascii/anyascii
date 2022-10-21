5570fe71396dcec1c779dd80cfa83ff1e7e4e4f0
hunterwb <hunter@hunterwb.com>
2022-10-20 17:24:07 -0700

    Update Latin

--- impl/php/_data/_1df.php
+++ impl/php/_data/_1df.php
@@ -1,1 +1,1 @@
-<?php return explode('	','fng	g	g	k	l	lzh	y	ng	r	t	q	qc	qc	t	x	q	k	l	dzh	l	ng	r	r	tsh	zh	dzh	i	o	tsh	c	s');
+<?php return explode('	','fng	g	g	k	l	lzh	y	ng	r	t	q	qc	qc	t	x	q	k	l	dzh	l	ng	r	r	tsh	zh	dzh	i	o	tsh	c	s							d	l	n	r	s	t');
