664b44b87f3718cfa454aa391eded418e5f16d22
Zane Huston <zane@dentimax.com>
2022-11-01 09:42:13 -0700

    feat: update Js generator to use cjs syntzx

--- src/main/java/com/anyascii/build/gen/Js.kt
+++ src/main/java/com/anyascii/build/gen/Js.kt
@@ -5,7 +5,7 @@ import kotlin.io.path.bufferedWriter
 
 fun js(g: Generator) {
     Path.of("impl/js/block.js").bufferedWriter().use { w ->
-        w.write("export default function block(blockNum) {\n")
+        w.write("module.exports = function block(blockNum) {\n")
         w.write("switch (blockNum) {\n")
         for ((blockNum, block) in g.blocks) {
             val s = block.noAscii().joinToString("\t").replace("\\", "\\\\").replace("\"", "\\\"")
