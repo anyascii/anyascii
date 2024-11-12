530afcccb3b9deca51e5da9d613a415a2376e06e
hunterwb <hunter@hunterwb.com>
2024-11-11 21:16:02 -0800

    Start Unicode 16

--- impl/php/_data/_10d.php
+++ impl/php/_data/_10d.php
@@ -1,1 +1,1 @@
-<?php return explode('	','	b	p	t	t	j	ch	h	kh	f	d	d	r	r	z	s	sh	k	g	l	m	n	w	u	y	i	ng	ny	v	a	i	u	e	o		m													0	1	2	3	4	5	6	7	8	9');
+<?php return explode('	','	b	p	t	t	j	ch	h	kh	f	d	d	r	r	z	s	sh	k	g	l	m	n	w	u	y	i	ng	ny	v	a	i	u	e	o		m													0	1	2	3	4	5	6	7	8	9							0	1	2	3	4	5	6	7	8	9');
--- impl/php/_data/_11b.php
+++ impl/php/_data/_11b.php
@@ -1,1 +1,1 @@
-<?php return explode('	','#	#	5	6	8	9	5	9	9	0');
+<?php return explode('	','#	#	5	6	8	9	5	9	9	0																																																																																																																																																																																																																																							0	1	2	3	4	5	6	7	8	9');
