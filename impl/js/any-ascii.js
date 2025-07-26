9832bcef8c95e098461536e34ca06534c809c252
hunterwb <hunter@hunterwb.com>
2025-07-26 00:36:22 -0700

    JavaScript: deduplicate strings

--- impl/js/any-ascii.js
+++ impl/js/any-ascii.js
@@ -1,6 +1,6 @@
 import block from './block.js';
 
-const blocks = {};
+const blocks = Object.create(null);
 
 /**
  * Transliterates a Unicode string into ASCII.
@@ -20,11 +20,25 @@ export default function anyAscii(string) {
 		const lo = codePoint & 0xff;
 		let b = blocks[blockNum];
 		if (b === undefined) {
-			blocks[blockNum] = b = block(blockNum).split("\t");
+			blocks[blockNum] = b = dedup(block(blockNum).split("\t"));
 		}
 		if (b.length > lo) {
 			result += b[lo];
 		}
 	}
 	return result;
+}
+
+function dedup(elems) {
+	const cache = Object.create(null);
+	for (let i = 0; i < elems.length; i++) {
+		const e = elems[i];
+		const c = cache[e];
+		if (c === undefined) {
+			cache[e] = e;
+		} else {
+			elems[i] = c;
+		}
+	}
+	return elems;
 }
