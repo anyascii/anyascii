e5366739a6511d3d3d2e6b9dcbe023ce684e6beb
hunterwb <hunter@hunterwb.com>
2025-09-10 21:09:11 -0700

    Start Sidetic

--- src/main/java/com/anyascii/build/Main.kt
+++ src/main/java/com/anyascii/build/Main.kt
@@ -254,6 +254,7 @@ private fun custom() = Table()
         .then(Table("tulu-tigalari"))
         .then(Table("todhri"))
         .then(Table("sutton-signwriting"))
+        .then(Table("sidetic"))
 
 private fun dominoes() = (0x1f030..0x1f093).toTable {
     val name = it.name.removePrefix("DOMINO TILE ")
