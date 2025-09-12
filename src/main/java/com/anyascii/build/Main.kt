3fe786205cc159f49a859201e06a6a57ecc6d648
hunterwb <hunter@hunterwb.com>
2025-09-12 13:12:09 -0700

    Start Tai Yo

--- src/main/java/com/anyascii/build/Main.kt
+++ src/main/java/com/anyascii/build/Main.kt
@@ -257,6 +257,7 @@ private fun custom() = Table()
         .then(Table("sidetic"))
         .then(Table("tolong-siki"))
         .then(Table("beria-erfe"))
+        .then(Table("tai-yo"))
 
 private fun dominoes() = (0x1f030..0x1f093).toTable {
     val name = it.name.removePrefix("DOMINO TILE ")
